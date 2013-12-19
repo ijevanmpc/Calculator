4a050cf866053f8018084db4ec3826e3aa1d10bc
artak <artak@artak.(none)>
2013-12-19 15:20:27 +0400

    fixed comments

--- Calc/src/com/example/calc/MainActivity.java
+++ Calc/src/com/example/calc/MainActivity.java
@@ -8,29 +8,41 @@ import android.widget.Button;
 import android.widget.EditText;
 
 /**
-* This activity is used to do regular math expressions with integer and float numbers....
-* 
-* @author Artak and Artush
-*/
+ * This activity is used to do regular math expressions with integer and float
+ * numbers....
+ * 
+ * @author Artak and Artush
+ */
 
 public class MainActivity extends Activity {
+
+	// buttons on calculator
 	Button one, two, tree, four, five, six, seven, eight, nine, zero, dot,
-			plus, minus, div, mult, clear, equal;// buttons on calculator
-	String str = ""; // string object, which will place values in EditText
-						// widget
-	Character operator = 'q';// math regular expression
-	float num, tempNum;// inserted numbers
-	EditText showResult;// a Edit text widget, where will enter numbers and show
-						// result
+			plus, minus, div, mult, clear, equal;
+
+	// string object, which will place values in EditText
+	// widget
+	String str = "";
+
+	// math regular expression
+	Character operator = 'q';
+
+	// inserted numbers
+	float num, tempNum;
+
+	// a Edit text widget, where will enter numbers and show result
+	EditText showResult;
+
 	/**
-	 * @brief - This function used to connect button click listeners to UI buttons
+	 * @brief - This function used to connect button click listeners to
+	 *        calculator's buttons
 	 * 
-	 
 	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
 		// connecting to views
 		one = (Button) findViewById(R.id.Button1);
 		two = (Button) findViewById(R.id.Button2);
@@ -52,7 +64,10 @@ public class MainActivity extends Activity {
 		equal = (Button) findViewById(R.id.ButtonEqual);
 		showResult = (EditText) findViewById(R.id.edit);
 
-		
+		/**
+		 * @brief - This function is button 1 on calculator
+		 * 
+		 */
 		one.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -61,6 +76,11 @@ public class MainActivity extends Activity {
 
 			}
 		});
+		
+		/**
+		 * @brief - This function is button 2 on calculator
+		 * 
+		 */
 		two.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -69,6 +89,11 @@ public class MainActivity extends Activity {
 
 			}
 		});
+		
+		/**
+		 * @brief - This function is button 3 on calculator
+		 * 
+		 */
 		tree.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -77,6 +102,11 @@ public class MainActivity extends Activity {
 
 			}
 		});
+		
+		/**
+		 * @brief - This function is button 4 on calculator
+		 * 
+		 */
 		four.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -85,6 +115,12 @@ public class MainActivity extends Activity {
 
 			}
 		});
+		
+		/**
+		 * @brief - This function is button 5 on calculator
+		 * 
+		 */
+
 		five.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -93,6 +129,11 @@ public class MainActivity extends Activity {
 
 			}
 		});
+		
+		/**
+		 * @brief - This function is button 6 on calculator
+		 * 
+		 */
 		six.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -101,6 +142,11 @@ public class MainActivity extends Activity {
 
 			}
 		});
+		
+		/**
+		 * @brief - This function is button 7 on calculator
+		 * 
+		 */
 		seven.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -109,6 +155,11 @@ public class MainActivity extends Activity {
 
 			}
 		});
+		
+		/**
+		 * @brief - This function is button 8 on calculator
+		 * 
+		 */
 		eight.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -117,6 +168,11 @@ public class MainActivity extends Activity {
 
 			}
 		});
+		
+		/**
+		 * @brief - This function is button 9 on calculator
+		 * 
+		 */
 		nine.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -125,6 +181,11 @@ public class MainActivity extends Activity {
 
 			}
 		});
+		
+		/**
+		 * @brief - This function is button 0 on calculator
+		 * 
+		 */
 		zero.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -134,6 +195,10 @@ public class MainActivity extends Activity {
 			}
 		});
 
+		/**
+		 * @brief - This function is button "." on calculator
+		 * 
+		 */
 		dot.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -142,6 +207,11 @@ public class MainActivity extends Activity {
 			}
 		});
 
+		
+		/**
+		 * @brief - This function is button "+" on calculator
+		 * 
+		 */
 		plus.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -150,6 +220,11 @@ public class MainActivity extends Activity {
 				operator = '+';
 			}
 		});
+		
+		/**
+		 * @brief - This function is button "-" on calculator
+		 * 
+		 */
 		minus.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -158,6 +233,11 @@ public class MainActivity extends Activity {
 				operator = '-';
 			}
 		});
+		
+		/**
+		 * @brief - This function is button "*" on calculator
+		 * 
+		 */
 		mult.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -166,6 +246,11 @@ public class MainActivity extends Activity {
 				operator = '*';
 			}
 		});
+		
+		/**
+		 * @brief - This function is button "/" on calculator
+		 * 
+		 */
 		div.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -174,6 +259,11 @@ public class MainActivity extends Activity {
 				operator = '/';
 			}
 		});
+		
+		/**
+		 * @brief - This function is button "=" on calculator
+		 * 
+		 */
 		equal.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -181,6 +271,11 @@ public class MainActivity extends Activity {
 				calculate();
 			}
 		});
+		
+		/**
+		 * @brief - This function is button "c" on calculator
+		 * 
+		 */
 		clear.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -190,11 +285,10 @@ public class MainActivity extends Activity {
 			}
 		});
 	}
-	
+
 	/**
 	 * @brief - This function reset values of all parameters
 	 * 
-	 
 	 */
 	private void reset() {
 		// TODO Auto-generated method stub
@@ -206,9 +300,9 @@ public class MainActivity extends Activity {
 	}
 
 	/**
-	 * @brief - This function used to insert clicked numbers value to EditText widget
+	 * @brief - This function used to insert clicked numbers value to EditText
+	 *        widget
 	 * 
-	 
 	 */
 	private void insert(String i) {
 		// TODO Auto-generated method stub
@@ -217,16 +311,21 @@ public class MainActivity extends Activity {
 		showResult.setText(str);
 
 	}
-
+	
+	/**
+	 * @brief - This function is perform inserted numbers and string 
+	 * 
+	 */
 	private void perform() {
 		// TODO Auto-generated method stub
 		str = "";
 		tempNum = num;
 	}
+
 	/**
-	 * @brief - This function used to do regular math operations with typed numbers
+	 * @brief - This function used to do regular math operations with typed
+	 *        numbers
 	 * 
-	 
 	 */
 	private void calculate() {
 		// TODO Auto-generated method stub
