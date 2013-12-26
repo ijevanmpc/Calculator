59158239e93280ee9243f4e097a1f24616d13ef4
artak <artak@artak.(none)>
2013-12-26 10:07:08 +0400

    final vesion

--- Calc/src/com/example/calc/MainActivity.java
+++ Calc/src/com/example/calc/MainActivity.java
@@ -7,7 +7,9 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-/**
+
+
+ /**
  * This activity is used to do regular math expressions with integer and float
  * numbers....
  * 
@@ -15,35 +17,14 @@ import android.widget.EditText;
  */
 
 public class MainActivity extends Activity {
-
-	// buttons on calculator
 	Button one, two, tree, four, five, six, seven, eight, nine, zero, dot,
 			plus, minus, div, mult, clear, equal;
-
-	// string object, which will place values in EditText
-	// widget
 	String str = "";
-
-	// math regular expression
 	Character operator = 'q';
-
-	// inserted numbers
 	float num, tempNum;
-
-	// a Edit text widget, where will enter numbers and show result
 	EditText showResult;
 
-	/**
-	 * @brief - This function used to connect button click listeners to
-	 *        calculator's buttons
-	 * 
-	 */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-		// connecting to views
+	private void initButtons() {
 		one = (Button) findViewById(R.id.Button1);
 		two = (Button) findViewById(R.id.Button2);
 		tree = (Button) findViewById(R.id.Button3);
@@ -63,227 +44,102 @@ public class MainActivity extends Activity {
 		zero = (Button) findViewById(R.id.Button0);
 		equal = (Button) findViewById(R.id.ButtonEqual);
 		showResult = (EditText) findViewById(R.id.edit);
+	}
 
-		/**
-		 * @brief - This function is button 1 on calculator
-		 * 
-		 */
-		one.setOnClickListener(new OnClickListener() {
+	private void setListeners() {
+		one.setOnClickListener(onClickListener);
+		two.setOnClickListener(onClickListener);
+		tree.setOnClickListener(onClickListener);
+		four.setOnClickListener(onClickListener);
+		five.setOnClickListener(onClickListener);
+		six.setOnClickListener(onClickListener);
+		seven.setOnClickListener(onClickListener);
+		eight.setOnClickListener(onClickListener);
+		nine.setOnClickListener(onClickListener);
+		zero.setOnClickListener(onClickListener);
+		dot.setOnClickListener(onClickListener);
+		plus.setOnClickListener(onClickListener);
+		minus.setOnClickListener(onClickListener);
+		div.setOnClickListener(onClickListener);
+		equal.setOnClickListener(onClickListener);
+		clear.setOnClickListener(onClickListener);
+	}
 
-			@Override
-			public void onClick(View arg0) {
+	private OnClickListener onClickListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.Button1:
 				insert("1");
-
-			}
-		});
-		
-		/**
-		 * @brief - This function is button 2 on calculator
-		 * 
-		 */
-		two.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
+				break;
+			case R.id.Button2:
 				insert("2");
-
-			}
-		});
-		
-		/**
-		 * @brief - This function is button 3 on calculator
-		 * 
-		 */
-		tree.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
+				break;
+			case R.id.Button3:
 				insert("3");
-
-			}
-		});
-		
-		/**
-		 * @brief - This function is button 4 on calculator
-		 * 
-		 */
-		four.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
+				break;
+			case R.id.Button4:
 				insert("4");
-
-			}
-		});
-		
-		/**
-		 * @brief - This function is button 5 on calculator
-		 * 
-		 */
-
-		five.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
+				break;
+			case R.id.Button5:
 				insert("5");
-
-			}
-		});
-		
-		/**
-		 * @brief - This function is button 6 on calculator
-		 * 
-		 */
-		six.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
+				break;
+			case R.id.Button6:
 				insert("6");
-
-			}
-		});
-		
-		/**
-		 * @brief - This function is button 7 on calculator
-		 * 
-		 */
-		seven.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
+				break;
+			case R.id.Button7:
 				insert("7");
-
-			}
-		});
-		
-		/**
-		 * @brief - This function is button 8 on calculator
-		 * 
-		 */
-		eight.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
+				break;
+			case R.id.Button8:
 				insert("8");
-
-			}
-		});
-		
-		/**
-		 * @brief - This function is button 9 on calculator
-		 * 
-		 */
-		nine.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
+				break;
+			case R.id.Button9:
 				insert("9");
-
-			}
-		});
-		
-		/**
-		 * @brief - This function is button 0 on calculator
-		 * 
-		 */
-		zero.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
+				break;
+			case R.id.Button0:
 				insert("0");
-
-			}
-		});
-
-		/**
-		 * @brief - This function is button "." on calculator
-		 * 
-		 */
-		dot.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
+				break;
+			case R.id.ButtonDot:
 				insert(".");
-			}
-		});
-
-		
-		/**
-		 * @brief - This function is button "+" on calculator
-		 * 
-		 */
-		plus.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				perform();
-				operator = '+';
-			}
-		});
-		
-		/**
-		 * @brief - This function is button "-" on calculator
-		 * 
-		 */
-		minus.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				perform();
-				operator = '-';
-			}
-		});
-		
-		/**
-		 * @brief - This function is button "*" on calculator
-		 * 
-		 */
-		mult.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				perform();
-				operator = '*';
-			}
-		});
-		
-		/**
-		 * @brief - This function is button "/" on calculator
-		 * 
-		 */
-		div.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
+				break;
+			case R.id.ButtonDiv:
 				perform();
 				operator = '/';
-			}
-		});
-		
-		/**
-		 * @brief - This function is button "=" on calculator
-		 * 
-		 */
-		equal.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
+				break;
+			case R.id.ButtonEqual:
 				calculate();
-			}
-		});
-		
-		/**
-		 * @brief - This function is button "c" on calculator
-		 * 
-		 */
-		clear.setOnClickListener(new OnClickListener() {
+				break;
+			case R.id.ButtonMin:
+				perform();
+				operator = '-';
+				break;
+			case R.id.ButtonMult:
+				perform();
+				operator = '*';
+				break;
+			case R.id.ButtonPlus:
+				perform();
+				operator = '+';
+				break;
 
-			@Override
-			public void onClick(View arg0) {
-				reset();
-
+			default:
+				break;
 			}
-		});
+
+		}
+	};
+
+	/**
+	 * @brief - This function used to connect button click listeners to UI
+	 *        buttons
+	 * 
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		initButtons();
+		setListeners();
 	}
 
 	/**
@@ -311,11 +167,7 @@ public class MainActivity extends Activity {
 		showResult.setText(str);
 
 	}
-	
-	/**
-	 * @brief - This function is perform inserted numbers and string 
-	 * 
-	 */
+
 	private void perform() {
 		// TODO Auto-generated method stub
 		str = "";
